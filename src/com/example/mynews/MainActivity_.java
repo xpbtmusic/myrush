05b6deedd1c871f5bade5d55bb3bac9420becf4e
xpbtmusic <xpbtmusic@163.com>
2013-01-31 16:23:57 +0800

    change

---
+++ src/com/example/mynews/MainActivity_.java
@@ -0,0 +1,96 @@
+package com.example.mynews;
+
+import org.holoeverywhere.addon.SlidingMenu.SlidingMenuA;
+import org.holoeverywhere.app.Activity;
+import org.holoeverywhere.app.Fragment;
+import org.holoeverywhere.widget.ListView;
+
+import com.slidingmenu.lib.SlidingMenu;
+
+import android.os.Bundle;
+
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class MainActivity_ extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		  Holo config = Holo.defaultConfig();
+	        config.requireSlidingMenu = true;
+	        init(config);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+		 final SlidingMenuA sm = requireSlidingMenu();
+	       
+	        sm.setContent(R.layout.content);
+	        sm.setBehindContentView(makeMenuView(savedInstanceState));
+	        
+	       sm.getSlidingMenu().setMode(SlidingMenu.LEFT);
+	        sm.getSlidingMenu().setBehindWidth(computeMenuWidth());
+	}
+
+	 public SlidingMenuA requireSlidingMenu() {
+	        return requireAddon(org.holoeverywhere.addon.SlidingMenu.class).activity(this);
+	    }
+	 private final class ListAdapter extends BaseAdapter{
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 9;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 9;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if(null==convertView){
+				convertView=getLayoutInflater().inflate(R.layout.menu_list_item);
+			}
+			return convertView;
+		}
+		 
+	 }
+	 private View makeMenuView(Bundle savedInstanceState) {
+	        View view = getLayoutInflater().inflate(R.layout.menu);
+	        
+	        ListView list = (ListView) view.findViewById(R.id.list);
+	        list.setAdapter(new ListAdapter());
+//	        list.setOnItemClickListener(navigationAdapter);
+	        return view;
+	    }
+	  public void replaceFragment(Fragment fragment) {
+	        replaceFragment(fragment, null);
+	    }
+
+	    public void replaceFragment(Fragment fragment,
+	            String backStackName) {
+	        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+	        ft.replace(R.id.content, fragment);
+	        if (backStackName != null) {
+	            ft.addToBackStack(backStackName);
+	        }
+	        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+	        ft.commit();
+	    }
+
+	    private int computeMenuWidth() {
+	        return (int) getResources().getFraction(R.dimen.demo_menu_width,
+	                getResources().getDisplayMetrics().widthPixels, 1);
+	    }
+}
---
+++ src/com/example/mynews/menu/MenuFragment.java
@@ -0,0 +1,68 @@
+
+package com.example.mynews.menu;
+
+import org.holoeverywhere.app.GridFragment;
+import org.holoeverywhere.widget.GridView;
+import com.example.mynews.R;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class MenuFragment extends GridFragment {
+  
+   
+ 
+
+	private final class menuAdapter extends BaseAdapter{
+
+		@Override
+		public int getCount() {
+			
+			return 10;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if(null==convertView){
+				convertView=getLayoutInflater().inflate(R.layout.menu_list_item);
+			}
+			return convertView;
+		}
+		
+	}
+   
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setGridAdapter(new menuAdapter());
+    }
+
+    @Override
+    public void onGridItemClick(GridView l, View v, int position, long id) {
+        
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getSupportActionBar().setSubtitle("Developers");
+    }
+
+   
+}
