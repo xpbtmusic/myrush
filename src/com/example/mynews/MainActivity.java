09f13bfc14551cde56f2198492466438de4ba72c
xpbtmusic <xpbtmusic@163.com>
2013-02-01 16:27:10 +0800

    add searchview

--- src/com/example/mynews/MainActivity.java
+++ src/com/example/mynews/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.mynews;
 import org.holoeverywhere.app.Activity;
 import org.holoeverywhere.app.Fragment;
 
+
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.widget.SearchView;
 import com.example.mynews.pageindicator.TestFragmentAdapter;
+import com.example.mynews.utils.StringUtils;
 import com.viewpagerindicator.TitlePageIndicator;
 
 import android.os.Bundle;
@@ -25,6 +30,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.simple_titles_bottom);
 		mAdapter = new TestFragmentAdapter(getSupportFragmentManager());
+		
 
 		mPager = (ViewPager) findViewById(R.id.pager);
 		mPager.setAdapter(mAdapter);
@@ -47,7 +53,27 @@ public class MainActivity extends Activity {
 	public void replaceFragment(Fragment fragment) {
 		replaceFragment(fragment, null);
 	}
+	
+	 @Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+	        //Used to put dark icons on light action bar
+	        boolean isLight = true;
 
+	        //Create the search view
+	        SearchView searchView = new SearchView(getSupportActionBar().getThemedContext());
+	        searchView.setQueryHint(StringUtils.getString(this, R.string.search_hint_text));
+
+//	        menu.add("Search")
+//	            .setIcon(isLight ? R.drawable.ic_search_inverse : R.drawable.abs__ic_search)
+//	            .setActionView(searchView)
+//	            .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+	        menu.add("Search")
+	        .setIcon(isLight ? R.drawable.ic_search_inverse : R.drawable.abs__ic_search)
+	        .setActionView(searchView)
+	        .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+	        return true;
+	    }
 	public void replaceFragment(Fragment fragment, String backStackName) {
 		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
 		ft.replace(R.id.content, fragment);
